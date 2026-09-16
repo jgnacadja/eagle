9c589656eaaa31759cc13bcbcda1683c8ba8271e
Freitas97 <freitasferre@gmail.com>
2026-09-16 15:03:59 +0100

    fix(front): correct test

--- apps/front/test/pages/rejoindre-le-reseau.spec.ts
+++ apps/front/test/pages/rejoindre-le-reseau.spec.ts
@@ -23,7 +23,6 @@ describe('Rejoindre le réseau page', () => {
     expect(wrapper.text()).toContain('Un réseau national de centres de formation')
     expect(wrapper.text()).toContain('Trois façons de rejoindre le réseau')
     expect(wrapper.text()).toContain('Ce que le réseau apporte')
-    expect(wrapper.text()).toContain("De la candidature à l'ouverture")
 
     const hrefs = wrapper.findAll('a').map((link) => link.attributes('href'))
     expect(hrefs).toContain('/centres/demande-de-formation?sujet=franchise')

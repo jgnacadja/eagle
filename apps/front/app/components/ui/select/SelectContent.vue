ce6963dce08b8833ee5093c67dcca685dc9b477a
Freitas97 <freitasferre@gmail.com>
2026-09-18 18:09:07 +0100

    fix(front): update correct pages

--- apps/front/app/components/ui/select/SelectContent.vue
+++ apps/front/app/components/ui/select/SelectContent.vue
@@ -6,20 +6,14 @@
         cn(
           'relative z-50 max-h-96 min-w-32 overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
           position === 'popper' &&
-            'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
+            'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1 min-w-(--reka-select-trigger-width)',
           props.class
         )
       "
     >
       <SelectScrollUpButton />
       <SelectViewport
-        :class="
-          cn(
-            'p-1',
-            position === 'popper' &&
-              'h-(--reka-select-trigger-height) w-full min-w-(--reka-select-trigger-width)'
-          )
-        "
+        :class="cn('p-1', position === 'popper' && 'w-full min-w-(--reka-select-trigger-width)')"
       >
         <slot />
       </SelectViewport>
@@ -44,6 +38,7 @@ const props = withDefaults(
   defineProps<SelectContentProps & { class?: HTMLAttributes['class'] }>(),
   {
     position: 'popper',
+    bodyLock: false,
     class: undefined
   }
 )
